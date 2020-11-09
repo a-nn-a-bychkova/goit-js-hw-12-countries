651ace09061cde38794cb09d2ac23ccff7409a25
Anna Bychkova <bychkova.dev@gmail.com>
2020-11-09 11:21:50 +0200

    changes js and html script

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,2 +1,7 @@
+var debounce = require('lodash.debounce');
 import API from './fetchCountries';
 import getRefs from './get-refs';
+
+API('uk').then(countries => {
+  console.log(countries);
+});
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 // Есть файл fetchCountries.js с дефолтным экспортом функции fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
 // import '../css/styles.css';
-const BASE_URL = 'https://restcountries.eu/rest/v2/';
+const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 function fetchCountries(searchQuery) {
   return fetch(`${BASE_URL}/name/${searchQuery}`).then(response =>
@@ -8,4 +8,4 @@ function fetchCountries(searchQuery) {
   );
 }
 
-export default { fetchCountries };
+export default fetchCountries;
